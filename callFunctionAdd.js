5287cebdef2fde3527d82038d3cfc331be7ddb93
FabulousDesigns <hazardmburu10@gmail.com>
2022-08-31 22:12:44 -0700

    Nodejs-modules

---
+++ callFunctionAdd.js
@@ -0,0 +1,6 @@
+// call function add and pass values to add
+
+let add = require('./functionAdd');
+const answer = add(40, 50);
+
+console.log(answer);
---
+++ functionAdd.js
@@ -0,0 +1,7 @@
+// function to add two values
+
+const add = (x,y)=>{
+    return x + y;
+}
+
+module.exports = add;
